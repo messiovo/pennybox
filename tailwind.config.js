322ce17313816a0808cc6894ff4878e49de529ef
messiovo@gmail.com <messiovo@gmail.com>
2024-09-20 10:09:35 +0100

    setting up vite and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#790997",
+        light:"#FEDCFE",
+        dark:"#701F58",
+        base:"#fffff"
+      }
+    },
+  },
+  plugins: [],
+}
