6e58dee0bc8be1f2a37c2d560f06ef4c0845619d
messiovo@gmail.com <messiovo@gmail.com>
2024-09-20 13:38:01 +0100

    edited mobile mood nav

--- src/Component/molecules/Nav.jsx
+++ src/Component/molecules/Nav.jsx
@@ -29,21 +29,21 @@ const Nav = () => {
 
   return (
     <div className="px-5 lg:px-14 py-5 flex items-center justify-between w-full  z-20 bg-primary">
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between gap-2">
             <img src={logo} alt="logo" className="lg:w-[2rem] w-[1.5rem]" />
-        <h1 className="text-white lg:text-2xl text-lg px-2">PennyBox</h1>
+        <h1 className="text-white lg:text-2xl text-lg">PennyBox</h1>
         </div>
        
       <div
         className={`${
           !isMobile ? `translate-y-0` : `translate-y-full `
-        } lg:scale-y-100 z-20 flex lg:flex-row flex-col  lg:gap-12 gap-20 lg:px-0 px-12 lg:py-0 py-12 lg:relative lg:w-auto w-full lg:h-auto  h-screen fixed  top-16  lg:inset-auto inset-0 lg:overflow-y-hidden overflow-y-visible lg:bg-[transparent] bg-primary text-center transition-transform duration-700 ease-in`}
+        } lg:scale-y-100 z-20 flex lg:flex-row flex-col  lg:gap-12 gap-20 lg:px-0 px-12 lg:py-0 py-12 lg:relative lg:w-auto w-full lg:h-auto  h-screen fixed  top-16  lg:inset-auto inset-0 lg:overflow-y-hidden overflow-y-visible bg-primary text-center transition-transform duration-700 ease-in`}
         onBlur={() => {
           setIsMobile(false);
         }}
-        tabIndex={30}
+        tabIndex={20}
       >
-        <div className="m-0 lg:m-auto  flex flex-col lg:flex-row lg:float:right lg:gap-14 gap-20">
+        <div className="m-0 lg:m-auto  flex flex-col lg:flex-row lg:float:right lg:gap-16 gap-20">
           {navLinks.map((items) => (
             <Links {...items} />
           ))}
@@ -55,8 +55,8 @@ const Nav = () => {
         onBlur={() => setIsMobile(false)}
         tabIndex={10}
       >
-        <span className={`${!isMobile ? "rotate-0" : "rotate-45"} w-full h-[1.5px] bg-white block transition-all duration-700 ease-in-out `}></span>
-        <span className={`${!isMobile ? "rotate-0" : "rotate-[-45deg] translate-y-[-7px]"} w-full h-[1.5px] bg-white block mt-1.5 transition-all duration-700 ease-in-out `}></span>
+        <span className={`${!isMobile ? "rotate-0" : "rotate-45"} w-full h-[2px] bg-white block transition-all duration-700 ease-in-out `}></span>
+        <span className={`${!isMobile ? "rotate-0" : "rotate-[-45deg] translate-y-[-7px]"} w-full h-[2px] bg-white block mt-1.5 transition-all duration-700 ease-in-out `}></span>
       </div>
     </div>
   );
