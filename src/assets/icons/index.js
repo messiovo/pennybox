b53042fbfe8909a4280a5925b28c933ff6865c7d
messiovo@gmail.com <messiovo@gmail.com>
2024-09-26 12:50:27 +0100

    building jsx icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,2 +1,6 @@
 export {default as Phone} from './Phone';
 export {default as Email} from './Email';
+export {default as Facebook} from './Facebook';
+export {default as Twitter} from './Twitter';
+export {default as Address} from './Address';
+export {default as Instagram } from './Instagram';
